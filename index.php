2bbfb96a60efc30c6852383a332da2763f2de454
ishidu-shinya <shinya.3268@gmail.com>
2019-09-17 12:34:09 +0900

    title modified

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
                         </p>
                         <p>[<a href="<?php echo get_template_directory_uri(); ?>/single.html">続きを読む</a>]</p>
                     </div>
-                    <figure><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a></figure>
+                    <figure><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/dummy/180x180-3.png" height="180" width="180" alt=""></a></figure>
                 </article><!-- /.news -->
                 <?php
                  endwhile;
